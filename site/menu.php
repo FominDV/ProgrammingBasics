6ade5bb180640146352405d71ef9c102b298efc9
Dmitriy Fomin <79067773397@yandex.ru>
2020-09-12 14:10:14 +0300

    add menu

---
+++ site/menu.php
@@ -0,0 +1,7 @@
+<div class="header">
+		<a href="index.php">Главная</a>
+		<a href="puzzle.php">Загадки</a>
+		<a href="guess.php">Угадайка соло</a>
+		<a href="guess2.php">Угадайка на двоих</a>
+		<a href="password.php">Генератор паролей</a>
+	</div>
